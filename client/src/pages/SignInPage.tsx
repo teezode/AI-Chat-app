3258b8a5ed7f2317dccc0df312a5d29e7677def5
John <olatunji.tjohn@gmail.com>
2025-06-13 13:23:27 +0100

    fefefefefe

--- client/src/pages/SignInPage.tsx
+++ client/src/pages/SignInPage.tsx
@@ -15,7 +15,7 @@ const SignInPage: React.FC = () => {
     setIsLoading(true);
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/signin', {
+      const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/auth/signin`, {
         email,
         password
       });
